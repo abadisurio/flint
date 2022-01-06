61365833b516c7043f794370a5e332abe96a4886
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-01-06 15:04:58 +0700

    flint: add theme data to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,65 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: RootPage(),
+    return MaterialApp(
+      title: 'Flint',
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xff073FCF),
+        appBarTheme: const AppBarTheme(backgroundColor: Color(0xff073FCF)),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(const Color(0xff073FCF)),
+          ),
+        ),
+        textTheme: const TextTheme(
+            headline1: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 40,
+                color: Color(0xff272727)),
+            headline2: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 35,
+                color: Color(0xff868686)),
+            headline3: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 30,
+            ),
+            headline4: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+                color: Color(0xFF000000)),
+            headline5: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 20,
+            ),
+            headline6: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 15,
+            ),
+            bodyText1: TextStyle(
+                fontWeight: FontWeight.w600, color: Color(0xff272727)),
+            // bodyText2: TextStyle(
+            //     fontWeight: FontWeight.w400,
+            //     fontSize: 15,
+            //     color: Color(0xff868686)),
+            button: TextStyle(fontWeight: FontWeight.w700)),
+      ),
+      // onGenerateRoute: (RouteSettings settings) {
+      //   switch (settings.name) {
+      //     case '/':
+      //       return CupertinoPageRoute(
+      //           builder: (_) => const BottomNavigation(), settings: settings);
+      //     case 'voting':
+      //       return CupertinoPageRoute(
+      //           builder: (_) => const Voting(), settings: settings);
+      //     case 'quick_action':
+      //       return CupertinoPageRoute(
+      //           builder: (_) => const QuickAction(), settings: settings);
+      //   }
+      // },
+      home: const RootPage(),
     );
   }
 }
