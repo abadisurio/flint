a2ae3efd36bc44138b8bf2ac99ceafb4bf6134bf
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-01-18 12:32:01 +0700

    flint: remove unused import

--- lib/dao/movie_dao.dart
+++ lib/dao/movie_dao.dart
@@ -4,10 +4,10 @@ import 'dart:math';
 
 import 'package:flint/database/database.dart';
 import 'package:flint/model/movie.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite/sqlite_api.dart';
+// import 'package:sqflite/sqlite_api.dart';
 
 class MovieDao {
   final dbProvider = DatabaseProvider.dbProvider;
