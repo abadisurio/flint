c85f6efaae0c3961afc7aa5656bb9d40115b5368
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-02-04 21:44:54 +0700

    flint: add userToken

---
+++ lib/database/userToken.dart
@@ -0,0 +1,17 @@
+import 'dart:developer';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TokenProvider {
+  static final TokenProvider tokenProvider = TokenProvider();
+
+  String? _token;
+
+  Future<String?> get token async {
+    if (_token != null) return _token;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString('authToken') ?? "";
+    // log("ini token1: " + _token.toString());
+    return _token;
+  }
+}
