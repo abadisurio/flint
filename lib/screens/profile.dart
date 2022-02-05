c466c3a5cc5158f3e7874e1cfb5bd5753524610f
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-02-05 14:03:35 +0700

    flint: remove unnecessary code from warnings

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -129,7 +129,7 @@ class _ProfilePageState extends State<ProfilePage> {
                           ],
                         ),
                       )))),
-          SliverFillRemaining(child: LikesPage())
+          const SliverFillRemaining(child: LikesPage())
         ],
       ),
     );
